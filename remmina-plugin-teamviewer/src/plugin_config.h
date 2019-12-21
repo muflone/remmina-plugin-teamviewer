701614dc17b79e12f643c16e469aa4cee5acc8bf
Muflone <webreg@vbsimple.net>
2019-12-21 03:31:59 +0100

    Updated for Builder 1.3.0.0

--- remmina-plugin-teamviewer/src/plugin_config.h
+++ remmina-plugin-teamviewer/src/plugin_config.h
@@ -19,11 +19,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef __PLUGIN_CONFIG_H
-  #define __PLUGIN_CONFIG_H
+#pragma once
 
-  #define PLUGIN_NAME        "TEAMVIEWER"
-  #define PLUGIN_DESCRIPTION "TEAMVIEWER - Launch a TeamViewer connection"
-  #define PLUGIN_VERSION     "1.2.3.0"
-  #define PLUGIN_APPICON     "remmina-teamviewer"
-#endif
+#define PLUGIN_NAME        "TEAMVIEWER"
+#define PLUGIN_DESCRIPTION "TEAMVIEWER - Launch a TeamViewer connection"
+#define PLUGIN_VERSION     "1.3.0.0"
+#define PLUGIN_APPICON     "remmina-teamviewer"
--- remmina-plugin-teamviewer/src/remmina_plugin.c
+++ remmina-plugin-teamviewer/src/remmina_plugin.c
@@ -21,80 +21,114 @@
 
 #include "plugin_config.h"
 #include <remmina/remmina_plugin.h>
-#if GTK_VERSION == 3
-  # include <gtk/gtkx.h>
-#endif
+#include <gtk/gtkx.h>
+
+typedef struct
+{
+  GtkTextView *text_view;
+  GtkTextBuffer *text_buffer;
+  GPid pid;
+} RemminaPluginData;
 
 static RemminaPluginService *remmina_plugin_service = NULL;
 
 static void remmina_plugin_teamviewer_init(RemminaProtocolWidget *gp)
 {
-  TRACE_CALL("remmina_plugin_teamviewer_init");
+  TRACE_CALL(__func__);
+  RemminaPluginData *gpdata;
   remmina_plugin_service->log_printf("[%s] Plugin init\n", PLUGIN_NAME);
+  /* Instance log window widgets */
+  gpdata = g_new0(RemminaPluginData, 1);
+  gpdata->text_view = GTK_TEXT_VIEW(gtk_text_view_new());
+  gtk_text_view_set_editable(gpdata->text_view, FALSE);
+  gtk_container_add(GTK_CONTAINER(gp), GTK_WIDGET(gpdata->text_view));
+  gpdata->text_buffer = gtk_text_view_get_buffer(gpdata->text_view);
+  gtk_text_buffer_set_text(gpdata->text_buffer, PLUGIN_DESCRIPTION, -1);
+  gtk_widget_show(GTK_WIDGET(gpdata->text_view));
+  /* Save reference to plugin data */
+  g_object_set_data_full(G_OBJECT(gp), "plugin-data", gpdata, g_free);
 }
 
 static gboolean remmina_plugin_teamviewer_open_connection(RemminaProtocolWidget *gp)
 {
-  TRACE_CALL("remmina_plugin_teamviewer_open_connection");
-  remmina_plugin_service->log_printf("[%s] Plugin open connection\n", PLUGIN_NAME);
+  TRACE_CALL(__func__);
+  RemminaFile *remminafile;
+  gboolean ret;
+  GError *error = NULL;
+  gchar *argv[50];  // Contains all the arguments included the password
+  gchar *argv_debug[50]; // Contains all the arguments, excluding the password
+  gchar *command_line; // The whole command line obtained from argv_debug
+  gint argc;
+  gint i;
+  gchar *option_str;
+  RemminaPluginData *gpdata;
+
   #define GET_PLUGIN_STRING(value) \
     g_strdup(remmina_plugin_service->file_get_string(remminafile, value))
   #define GET_PLUGIN_BOOLEAN(value) \
     remmina_plugin_service->file_get_int(remminafile, value, FALSE)
-  #define GET_PLUGIN_INT(value, default_value) \
-    remmina_plugin_service->file_get_int(remminafile, value, default_value)
-  #define GET_PLUGIN_PASSWORD(value) \
-    g_strdup(remmina_plugin_service->file_get_secret(remminafile, value));
+  #define ADD_ARGUMENT(name, value) \
+    { \
+      argv[argc] = g_strdup(name); \
+      argv_debug[argc] = g_strdup(name); \
+      argc++; \
+      if (value != NULL) { \
+        argv[argc] = value; \
+        argv_debug[argc++] = g_strdup(g_strcmp0(name, "-p") != 0 ? value : "XXXXX"); \
+      } \
+    }
 
-  RemminaFile *remminafile;
-  gboolean ret;
-  GError *error = NULL;
-  gchar *argv[50];
-  gint argc;
-  gint i;
-  GPid pid;
-  gchar *option_str;
-
+  remmina_plugin_service->log_printf("[%s] Plugin open connection\n", PLUGIN_NAME);
   remminafile = remmina_plugin_service->protocol_plugin_get_file(gp);
 
+  gpdata = (RemminaPluginData*) g_object_get_data(G_OBJECT(gp), "plugin-data");
+
   argc = 0;
-  argv[argc++] = strdup("teamviewer");
+  // Main executable name
+  ADD_ARGUMENT("teamviewer", NULL);
 
   // Some tvw_main skips the first argument so we're adding a fake argument in the first place
   if (GET_PLUGIN_BOOLEAN("adddashes"))
-    argv[argc++] = g_strdup("--");
-  argv[argc++] = g_strdup("-i");
-  argv[argc++] = GET_PLUGIN_STRING("server");;
-
-  option_str = GET_PLUGIN_PASSWORD("password");
+  {
+    ADD_ARGUMENT("--", NULL);
+  }
+  // Server address
+  option_str = GET_PLUGIN_STRING("server");
+  ADD_ARGUMENT("-i", option_str);
+  // The password to authenticate with
+  option_str = GET_PLUGIN_STRING("password");
   if (option_str)
   {
-    argv[argc++] = g_strdup("--Password");
-    argv[argc++] = g_strdup(option_str);
+    ADD_ARGUMENT("--Password", option_str);
   }
-
-  argv[argc++] = NULL;
-
-  remmina_plugin_service->log_printf("[TEAMVIEWER] starting teamviewer\n");
-  ret = g_spawn_async(NULL, argv, NULL, G_SPAWN_SEARCH_PATH,
-    NULL, NULL, &pid, &error);
-  remmina_plugin_service->log_printf("[TEAMVIEWER] started with pid %d\n", 
-    &pid);
-
+  // End of the arguments list
+  ADD_ARGUMENT(NULL, NULL);
+  // Retrieve the whole command line
+  command_line = g_strjoinv(g_strdup(" "), (gchar **)&argv_debug[0]);
+  remmina_plugin_service->log_printf("[TEAMVIEWER] starting %s\n", command_line);
+  g_free(command_line);
+  // Execute the external process rdesktop
+  ret = g_spawn_async(NULL, argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, &gpdata->pid, &error);
+  remmina_plugin_service->log_printf("[TEAMVIEWER] started teamviewer with GPid %d\n", &gpdata->pid);
+  // Free the arguments list
   for (i = 0; i < argc; i++)
+  {
+    g_free(argv_debug[i]);
     g_free(argv[i]);
-
+  }
+  // Show error message
   if (!ret)
+  {
     remmina_plugin_service->protocol_plugin_set_error(gp, "%s", error->message);
-
-  return FALSE;
+  }
+  remmina_plugin_service->protocol_plugin_signal_connection_opened(gp);
+  return TRUE;
 }
 
-static gboolean remmina_plugin_teamviewer_close_connection(RemminaProtocolWidget *gp)
-{
-  TRACE_CALL("remmina_plugin_teamviewer_close_connection");
+static gboolean remmina_plugin_teamviewer_close_connection(RemminaProtocolWidget *gp) {
+  TRACE_CALL(__func__);
   remmina_plugin_service->log_printf("[%s] Plugin close connection\n", PLUGIN_NAME);
-  remmina_plugin_service->protocol_plugin_emit_signal(gp, "disconnect");
+  remmina_plugin_service->protocol_plugin_signal_connection_closed(gp);
   return FALSE;
 }
 
@@ -105,12 +139,12 @@ static gboolean remmina_plugin_teamviewer_close_connection(RemminaProtocolWidget
  * c) Setting description
  * d) Compact disposition
  * e) Values for REMMINA_PROTOCOL_SETTING_TYPE_SELECT or REMMINA_PROTOCOL_SETTING_TYPE_COMBO
- * f) Unused pointer
+ * f) Setting tooltip
  */
 static const RemminaProtocolSetting remmina_plugin_teamviewer_basic_settings[] =
 {
-  { REMMINA_PROTOCOL_SETTING_TYPE_SERVER, NULL, NULL, FALSE, NULL, NULL },
-  { REMMINA_PROTOCOL_SETTING_TYPE_PASSWORD, NULL, NULL, FALSE, NULL, NULL },
+  { REMMINA_PROTOCOL_SETTING_TYPE_SERVER, "server", NULL, FALSE, NULL, NULL },
+  { REMMINA_PROTOCOL_SETTING_TYPE_PASSWORD, "password", N_("User password"), FALSE, NULL, NULL },
   { REMMINA_PROTOCOL_SETTING_TYPE_CHECK, "adddashes", N_("Add dashes as first argument"), FALSE, NULL, NULL },
   { REMMINA_PROTOCOL_SETTING_TYPE_END, NULL, NULL, FALSE, NULL, NULL }
 };
@@ -135,16 +169,15 @@ static RemminaProtocolPlugin remmina_plugin =
   NULL,                                         // Query for available features
   NULL,                                         // Call a feature
   NULL,                                         // Send a keystroke
-  NULL                                          // Capture screenshot
+  NULL                                          // Screenshot support
 };
 
 G_MODULE_EXPORT gboolean remmina_plugin_entry(RemminaPluginService *service)
 {
-  TRACE_CALL("remmina-plugin-teamviewer::remmina_plugin_entry");
+  TRACE_CALL(__func__);
   remmina_plugin_service = service;
 
-  if (!service->register_plugin((RemminaPlugin *) &remmina_plugin))
-  {
+  if (!service->register_plugin((RemminaPlugin *) &remmina_plugin)) {
     return FALSE;
   }
   return TRUE;
